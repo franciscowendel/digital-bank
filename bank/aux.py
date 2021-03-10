2b8fe2aae39efdd8be1051a23a5080259bf5c659
franciscowendel <iamfranciscowendel@gmail.com>
2021-03-10 18:23:10 -0300

    Implementando o arquivo 'aux.py'

--- bank/aux.py
+++ bank/aux.py
@@ -4,3 +4,11 @@ from datetime import date
 
 def float_to_str(valor) -> str:
     return f'R$ {valor:,.2f}'
+
+
+def date_to_str(data) -> str:
+    return data.strftime('%d/%m/%Y')
+
+
+def str_to_date(data) -> date:
+    return datetime.strptime(data, '%d/%m/%Y')
