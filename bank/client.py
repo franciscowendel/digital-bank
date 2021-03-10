c95aa8299046e884a7f2287a40bed01551cca07b
franciscowendel <iamfranciscowendel@gmail.com>
2021-03-10 20:51:26 -0300

    Implementando o arquivo 'client.py'

---
+++ bank/client.py
@@ -0,0 +1,19 @@
+from aux import date_to_str
+from aux import float_to_str
+from aux import str_to_date
+from datetime import date
+
+
+class Client:
+
+    contador: int = 100
+
+    def __init__(self, nome: str, sobrenome: str, email: str, cpf: str, data_nascimento: str):
+        self.__codigo: int = Client.contador
+        self.___nome: str = nome
+        self.__sobrenome: str = sobrenome
+        self.__email: str = email
+        self.__cpf: str = cpf
+        self.__data_nascimento: date = str_to_date(data_nascimento)
+        self.__data_cadastro: date = date.today()
+
