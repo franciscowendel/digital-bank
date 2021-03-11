c3d07cedf46d180b2994f30399900086bdbfdbb1
franciscowendel <iamfranciscowendel@gmail.com>
2021-03-10 22:50:33 -0300

    Implementando o arquivo 'bank.py'

--- bank/bank.py
+++ bank/bank.py
@@ -11,5 +11,28 @@ def menu():
     pass
 
 
+def criar_conta():
+    pass
+
+def efetuar_deposito():
+    pass
+
+
+def efetuar_saque():
+    pass
+
+
+def efetuar_transferencia():
+    pass
+
+
+def listar_contas():
+    pass
+
+
+def rastrear_conta(numero):
+    pass
+
+
 if __name__ == '__main__':
     menu()
