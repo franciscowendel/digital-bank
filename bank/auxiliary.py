023f65623907e102000468d9d370dbcbcd938f1c
Francisco <iamfranciscowendel@gmail.com>
2024-01-12 14:18:06 -0300

    Update auxiliary.py

--- bank/auxiliary.py
+++ bank/auxiliary.py
@@ -2,13 +2,16 @@ from datetime import datetime
 from datetime import date
 
 
-def float_to_str(valor) -> str:
-    return f'R$ {valor:,.2f}'
+def float_to_str(value) -> str:
+    """Transforms a float value into a string."""
+    return f'R$ {value:,.2f}'
 
 
 def date_to_str(data) -> str:
+    """Transforms a date object into a string."""
     return data.strftime('%d/%m/%Y')
 
 
 def str_to_date(data) -> date:
+    """Tranforms a string into a date object."""
     return datetime.strptime(data, '%d/%m/%Y')
