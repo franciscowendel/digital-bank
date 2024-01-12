3bcc798f16a8abe0e6d5c598a96992527be01a9e
Francisco <iamfranciscowendel@gmail.com>
2024-01-12 14:25:29 -0300

    Translating the project 'digital bank' into english.

--- bank/client.py
+++ bank/client.py
@@ -1,20 +1,24 @@
-from aux import date_to_str, str_to_date
+from aux import (
+    date_to_str, 
+    str_to_date
+)
+
 from datetime import date
 
 
 class Client:
 
-    contador = 100
+    number: int = 1
 
-    def __init__(self, name: str, last_name: str, email: str, cpf: str, data_nascimento: str) -> None:
-        self.__code: int = Client.contador
+    def __init__(self, name: str, last_name: str, email: str, cpf: str, birthday: str) -> None:
+        self.__code: int = Client.number
         self.__name: str = name
         self.__last_name: str = last_name
         self.__email: str = email
         self.__cpf: str = cpf
-        self.__data_nascimento: date = str_to_date(data_nascimento)
-        self.__data_cadastro: date = date.today()
-        Client.contador += 1
+        self.__birthday: date = str_to_date(birthday)
+        self.__day_of_registration: date = date.today()
+        Client.number += 1
 
     @property
     def code(self) -> int:
@@ -37,15 +41,15 @@ class Client:
         return self.__cpf
 
     @property
-    def data_nascimento_ed(self) -> str:
-        return date_to_str(self.__data_nascimento)
+    def birthday_edited(self) -> str:
+        return date_to_str(self.__birthday)
 
     @property
-    def data_cadastro_ed(self) -> str:
-        return date_to_str(self.__data_cadastro)
+    def day_of_registration_edited(self) -> str:
+        return date_to_str(self.__day_of_registration)
 
     def __str__(self) -> str:
         return f"Client's code: {self.code}\nCLient's name: {self.name}\nClient's last name: {self.last_name}" \
                f"\nClient's email: {self.email}\nClient's cpf: {self.cpf}"\
-               f"\nClient's birthday: {self.data_nascimento_ed}" \
-               f"\nClient's day of registration: {self.data_cadastro_ed}"
+               f"\nClient's birthday: {self.birthday_edited}" \
+               f"\nClient's day of registration: {self.day_of_registration_edited}"
